b8f3f25fb8df77bb94ed1af10bba2f0c7040b1cd
Ivan Longarai <ivanslongarai@gmail.com>
2023-05-13 15:02:03 -0300

    Developed the Update and Delete endpoint for the Supplier Entity

--- Program.cs
+++ Program.cs
@@ -64,10 +64,72 @@ app.MapPost("/supplier", async (MinimalContextDb ctx, Supplier supplier) =>
             return Results.ValidationProblem(result);
         }
     })
-    .ProducesValidationProblem()
-    .Produces<Supplier>(StatusCodes.Status201Created)
-    .Produces(StatusCodes.Status400BadRequest)
-    .WithName("PostSupplier")
-    .WithTags("Supplier");
+.ProducesValidationProblem()
+.Produces<Supplier>(StatusCodes.Status201Created)
+.Produces(StatusCodes.Status400BadRequest)
+.WithName("PostSupplier")
+.WithTags("Supplier");
+
+app.MapPut("/supplier/{id}", async (Guid id, MinimalContextDb ctx, Supplier supplier) =>
+{
+        var supplierStored = await ctx.Suppliers.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+
+        if (supplierStored == null)
+        {
+            return Results.NotFound();
+        }
+        else
+        {
+            supplier.Id = id;
+        }
+
+        var validationResult = Supplier.GetValidator().Validate(supplier);
+
+        if (validationResult.IsValid)
+        {
+            ctx.Suppliers.Update(supplier);
+            var result = await ctx.SaveChangesAsync();
+            return result > 0 ? Results.Ok(supplier) : Results.BadRequest("Something went wrong");
+        }
+        else
+        {
+            var errosToReturn = validationResult.Errors.Select(e => e.ErrorMessage);
+
+            var result = new Dictionary<string, string[]>
+            {
+                { "Supplier", errosToReturn.ToArray() }
+            };
+
+            return Results.ValidationProblem(result);
+        }
+})
+.ProducesValidationProblem()
+.Produces<Supplier>(StatusCodes.Status200OK)
+.Produces(StatusCodes.Status404NotFound)
+.Produces(StatusCodes.Status400BadRequest)
+.WithName("PutSupplier")
+.WithTags("Supplier");
+
+app.MapDelete("/supplier/{id}", async (Guid id, MinimalContextDb ctx) =>
+{
+    var supplierStored = await ctx.Suppliers.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+
+    if (supplierStored == null)
+    {
+        return Results.NotFound();
+    }
+    else
+    {
+        ctx.Suppliers.Remove(supplierStored);
+        var result = await ctx.SaveChangesAsync();
+        return result > 0 ? Results.Ok($"Removed successfully the supplier id: {id}") : Results.BadRequest("Something went wrong");
+    }    
+})
+.ProducesValidationProblem()
+.Produces<Supplier>(StatusCodes.Status200OK)
+.Produces(StatusCodes.Status404NotFound)
+.Produces(StatusCodes.Status400BadRequest)
+.WithName("DeleteSupplier")
+.WithTags("Supplier");
 
 app.Run();
